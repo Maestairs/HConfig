a394016a9ad058fcb0d4db60985d8facb392ddfc
Maestairs <Andrew.Torrance.Public@Googlemail.com>
2016-11-17 12:52:52 +0000

    Code tidy up

--- HConfig/ICacheableConfigController.cs
+++ HConfig/ICacheableConfigController.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HConfig
+﻿namespace HConfig
 {
-    interface ICacheableConfigController:IConfigController
+    public interface ICacheableConfigController:IConfigController
     {
         IConfigCache Cache { get; set; }
-    
     }
 }
--- HConfigTests/CachedConfigControllerTests.cs
+++ HConfigTests/CachedConfigControllerTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using HConfig;
 using NUnit.Framework;
 
 namespace HConfigTests
